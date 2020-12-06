3afd63e6c856606542dfbbadbc9d13eb9409e4ec
Manish Raj <42971673+manishrajmishra@users.noreply.github.com>
2020-12-06 15:39:57 +0530

    feat: added the functionalities of like counter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
-// var userName = "Manish Raj";
-var userName = prompt("Give me Your Name");
-
 export default function App() {
+  const [counter, setlikeCounter] = useState(0);
+
+  function likeClickHandler() {
+    var newcounter = counter + 1;
+    // console.log(counter);
+    setlikeCounter(newcounter);
+  }
   return (
     <div className="App">
-      <h1 style={{ color: "blue" }}>
-        Welcome <span style={{ color: "green" }}>{userName}</span>
-      </h1>
+      <h1>insiide outtt</h1>
+      <button onClick={likeClickHandler}>Like Me! </button> {counter}
     </div>
   );
 }
