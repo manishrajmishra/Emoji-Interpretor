f5205f9bbbb6a9a7dcee286610a3de646f963d9a
Manish Raj <42971673+manishrajmishra@users.noreply.github.com>
2020-12-06 15:22:23 +0530

    testing how promt works in jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./styles.css";
 
-var userName = "Manish Raj";
+// var userName = "Manish Raj";
+var userName = prompt("Give me Your Name");
 
 export default function App() {
   return (
     <div className="App">
-      <h1 style={{ color: "blue" }}>Welcome {userName}</h1>
+      <h1 style={{ color: "blue" }}>
+        Welcome <span style={{ color: "green" }}>{userName}</span>
+      </h1>
     </div>
   );
 }
