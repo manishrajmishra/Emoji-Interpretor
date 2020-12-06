6900fd5cff7442ebb44bc44d0436fcca100da4c4
Manish Raj <42971673+manishrajmishra@users.noreply.github.com>
2020-12-06 15:47:30 +0530

    feat: add new like counter button live excercise at neog.camp

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import "./styles.css";
 export default function App() {
   const [counter, setlikeCounter] = useState(0);
 
+  const [counter2, setlikeCounter2] = useState(0);
+
+  function likeClickHandler2() {
+    var newcount = counter2 + 1;
+    setlikeCounter2(newcount);
+  }
   function likeClickHandler() {
     var newcounter = counter + 1;
     setlikeCounter(newcounter);
@@ -11,6 +17,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>insiide outtt</h1>
+      <button onClick={likeClickHandler2}>Like Me 2</button> {counter2}
       <button onClick={likeClickHandler}>Like Me! </button> {counter}
     </div>
   );
