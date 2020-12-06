c658e6b56c876fc14e86a00ed641332bb7d9eb1d
Manish Raj <42971673+manishrajmishra@users.noreply.github.com>
2020-12-06 16:53:58 +0530

    feat: added the view

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  var [userInput, setUserInput] = useState("");
   function inputChangeHandler(event) {
     console.log(event.target.value);
+    setUserInput(event.target.value);
   }
   return (
     <div className="App">
       <h1>insiide outtt</h1>
       <input onChange={inputChangeHandler}></input>
+      <div>{userInput}</div>
     </div>
   );
 }
