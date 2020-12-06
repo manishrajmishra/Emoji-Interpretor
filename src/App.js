0cf02960c2092c6f10f5f81af3d020ac7d5e74be
Manish Raj <42971673+manishrajmishra@users.noreply.github.com>
2020-12-06 15:09:42 +0530

    testing some functionalities

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./styles.css";
 
+var headingText = "insiide outtt";
+
 export default function App() {
   return (
     <div className="App">
-      <h1>insiide outtt</h1>
+      <h1>{headingText}</h1>
     </div>
   );
 }
