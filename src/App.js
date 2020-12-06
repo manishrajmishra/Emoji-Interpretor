92efe7509ff7d94590b07febe647072bf4995c57
Manish Raj <42971673+manishrajmishra@users.noreply.github.com>
2020-12-06 16:46:53 +0530

    feat: added the functionalities of onChange

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import "./styles.css";
 
 export default function App() {
-  const [counter, setlikeCounter] = useState(0);
-
-  const [counter2, setlikeCounter2] = useState(0);
-
-  function likeClickHandler2() {
-    var newcount = counter2 + 1;
-    setlikeCounter2(newcount);
-  }
-  function likeClickHandler() {
-    var newcounter = counter + 1;
-    setlikeCounter(newcounter);
+  function inputChangeHandler(event) {
+    console.log(event.target.value);
   }
   return (
     <div className="App">
       <h1>insiide outtt</h1>
-      <button onClick={likeClickHandler2}>Like Me 2</button> {counter2}
-      <button onClick={likeClickHandler}>Like Me! </button> {counter}
+      <input onChange={inputChangeHandler}></input>
     </div>
   );
 }
