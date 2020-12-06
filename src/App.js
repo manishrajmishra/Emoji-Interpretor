37095937c4316be869b2cb14329540198b02b550
Manish Raj <42971673+manishrajmishra@users.noreply.github.com>
2020-12-06 17:51:12 +0530

    feat: added the emoji indentifier, styled and everything

--- src/App.js
+++ src/App.js
@@ -1,17 +1,59 @@
 import React, { useState } from "react";
 import "./styles.css";
 
+var emojiDictionary = {
+  "😊": "smiling",
+  "😳": "disbelief",
+  "😔": "sad",
+  "🥡": "takeout box",
+  "❤️": "love",
+  "😑": "annoyance",
+  "😍": "smiling face with heart-eyes",
+  "😘": "face blowing a kiss",
+  "😚": "kissing face with closed eyes",
+  "💣": "bomb",
+  "👋": "waving hand",
+  "👏": "clapping hands",
+  "🙌": "raising hands",
+  "🤝": "handsake"
+};
+
+var emojisWeKnow = Object.keys(emojiDictionary);
+
 export default function App() {
-  var [userInput, setUserInput] = useState("");
-  function inputChangeHandler(event) {
-    console.log(event.target.value);
-    setUserInput(event.target.value);
+  const [meaning, setMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+
+    var meaning = emojiDictionary[userInput];
+
+    if (meaning === undefined) {
+      meaning = "We don't have this in our database";
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
   }
   return (
     <div className="App">
-      <h1>insiide outtt</h1>
-      <input onChange={inputChangeHandler}></input>
-      <div>{userInput}</div>
+      <h1>inside outt!</h1>
+
+      <input onChange={emojiInputHandler} />
+
+      <h2> {meaning} </h2>
+
+      <h3> Emojis in Our Database are listed Below </h3>
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span onClick={() => emojiClickHandler(emoji)} key={emoji}>
+            {emoji}
+          </span>
+        );
+      })}
     </div>
   );
 }
